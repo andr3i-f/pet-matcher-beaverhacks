105d71225e024cfe0474cd1c52d9183304e46ab2
andr3if <andrei.florea003@gmail.com>
2025-04-06 09:26:02 -0700

    hi

--- src/app/form/FormProvider.tsx
+++ src/app/form/FormProvider.tsx
@@ -5,7 +5,7 @@ import { submitSelectedImages } from "../api/form";
 import { LocationContext } from "../../../context/LocationProvider";
 import { useRouter } from 'next/navigation';
 import { MatchContext } from "../../../context/MatchContext";
-const steps = ["Step 1", "Step 2"]
+const steps = ["Step 1", "Step 2", "Step 3", "Step 4", "Step 5", "Step 6", "Step 7", "Step 8", "Step 9", "Step 10"];
 
 const listImages = [
     "https://images.pexels.com/photos/126407/pexels-photo-126407.jpeg",
