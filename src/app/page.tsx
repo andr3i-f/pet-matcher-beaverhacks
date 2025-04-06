d2bfe6271a10a5c9456ab4986782da41ef027012
andr3if <andrei.florea003@gmail.com>
2025-04-05 17:08:42 -0700

    initial landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,37 @@
+import { Box, Button, Stack, Typography } from "@mui/material";
+
 export default function Home() {
   return (
-    <h1>
-      Landing page...
-    </h1>
+    <Box
+      sx={{
+        width: "100%",
+        height: "100vh",
+        display: "flex",
+        alignItems: "center", 
+        justifyContent: "center",
+        backgroundImage: `url("/images/background.png")`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat"
+      }}
+    >
+      <Stack
+        direction={"column"}
+        alignItems={"center"}
+      >
+        <Typography variant="h1" sx={{ color: 
+          "#660F81",
+          fontWeight: "bold"
+         }}>Pawfect Match</Typography>
+        <Button variant="contained" sx={{
+          width: "40%",
+          backgroundColor: "#65548f",
+          color: "#fff",      
+          '&:hover': {
+            backgroundColor: "#7965ab"
+          }
+        }} >find my fur friend</Button>
+      </Stack>
+    </Box>
   );
 }
